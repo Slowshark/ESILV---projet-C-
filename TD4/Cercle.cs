73773083e6d6ad9676a495296161aaf578325bb4
Louja971 <mcjael971@gmail.com>
2026-04-21 20:01:01 +0200

    fzf

--- TD4/Cercle.cs
+++ TD4/Cercle.cs
@@ -8,18 +8,21 @@ namespace TD4
 {
     public class Cercle : Forme
     {
+        // Coordonnées du centre du cercle
         double cx;
         double cy;
-        double r;
+        double r; // Rayon du cercle
         public Cercle(int idelement, int ordre, int R, int G, int B, double cx, double cy, double r)
             : base(idelement, ordre, R, G, B)
         {
+            // Initialisation des paramètres
             this.cx = cx;
             this.cy = cy;
             this.r = r;
         }
         public override string GenererBaliseSvg()
         {
+            // Génération de la balise SVG du cercle avec ses propriétés
             return $"  <circle cx=\"{cx}\" cy=\"{cy}\" r=\"{r}\" style=\"fill:rgb({R},{G},{B})\"{GenererTransformSvg()} />";
         }
     }
