e5d2d7251b16b151c51496ad5a911d0dd526a32c
Yvan <ung.thivong91@gmail.com>
2026-04-21 20:06:03 +0200

    f

--- TD4/Texte.cs
+++ TD4/Texte.cs
@@ -8,19 +8,20 @@ namespace TD4
 {
     public class Texte : Forme
     {
+        // Position et contenu du texte
         double x;
         double y;
         string contenu;
         public Texte(int idelement, int ordre, int R, int G, int B, double x, double y, string contenu)
             : base(idelement, ordre, R, G, B)
         {
+            // Initialisation du texte
             this.x = x;
             this.y = y;
             this.contenu = contenu;
         }
         public override string GenererBaliseSvg()
-        {
-            return $"  <text x=\"{x}\" y=\"{y}\" fill=\"rgb({R},{G},{B})\"{GenererTransformSvg()}>{contenu}</text>";
+        {            // Génération de la balise SVG text            return $"  <text x=\"{x}\" y=\"{y}\" fill=\"rgb({R},{G},{B})\"{GenererTransformSvg()}>{contenu}</text>";
         }
     }
 }
